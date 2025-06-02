53a3c166825dd700b909819d6dedb91697694585
ReencH6678 <ddaammiirr6678@gmail.com>
2025-06-02 19:31:33 +0200

    Corutine fix

--- Assets/Scrtpts/Cube.cs
+++ Assets/Scrtpts/Cube.cs
@@ -8,7 +8,7 @@ public class Cube : MonoBehaviour
     private float _maxLifeTime = 5;
     private float _minLifeTime = 2;
 
-    private bool _haveColorChanged = false;
+    private bool _haveCollised = false;
 
     private ColorChanger _colorChanger;
     private Rigidbody _rigidbody;
@@ -23,29 +23,31 @@ public class Cube : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.TryGetComponent<Platform>(out Platform platform))
+        if (collision.gameObject.TryGetComponent<Platform>(out Platform platform) && _haveCollised == false)
+        {
             StartCoroutine(DeteteObject());
+            _haveCollised = true;
+        }
     }
 
     public void ResetObject()
     {
         _colorChanger.ResetColor();
+
         _rigidbody.velocity = Vector3.zero;
         _rigidbody.angularVelocity = Vector3.zero;
+
+        transform.position = Vector3.zero;
         transform.rotation = Quaternion.identity;
     }
 
     private IEnumerator DeteteObject()
     {
-        if (_haveColorChanged == false)
-        {
-            _colorChanger.ChangeColor();       
-            _haveColorChanged = true;
-        }
+        _colorChanger.ChangeColor();
 
         yield return new WaitForSeconds(Random.Range(_minLifeTime, _maxLifeTime));
 
-        _haveColorChanged = false;
+        _haveCollised = false;
         Collised?.Invoke(this);
     }
 }
